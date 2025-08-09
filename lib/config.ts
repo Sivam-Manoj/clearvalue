d3286c6f6e09a9fc1c333ceae5f190a2b7fe1d0b
skodelabs <office@skodelabs.com>
2025-08-09 21:05:18 +0530

    server update

--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "https://api.clearvalue.site";
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "https://api.clearvalue.site/api";
